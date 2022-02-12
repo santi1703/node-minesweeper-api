32189f2d38e822901a768237c47f78e5adc9cb0f
= <=>
2022-02-12 04:02:43 -0300

    Included Express for better http methods code

--- index.js
+++ index.js
@@ -1,36 +1,28 @@
-var http = require('http');
-var mongo = require('mongodb');
-var url = require('url');
 const { parse } = require('querystring');
 const { ObjectId } = require('mongodb');
-var { MineField } = require('./mines.js');
+const { MineField } = require('./mines.js');
 
+const mongo = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = "mongodb://localhost:27017/";
 const mongoBase = "minesweeper";
 const mongoCollection = "minefields";
 
+const express = require('express');
+const app = express();
+const port = 80;
+app.use(express.json());
+
 // This attribute should determine if the fetched MineField was just created or if it is loaded from storage
 var fresh = {isFresh: true};
 var status = {status: "unmodified"};
 
-http.createServer(function (req, res) {
-  const { method } = req;
-  res.writeHead(200, {'Content-Type': 'application/json'});
+app.get('/:minefieldId?', function(req, res) {
+  let documentId = req.params['minefieldId'] ? ObjectId(req.params['minefieldId']) : null;
   
-  // Gets the id of the minefield to load or save
-  var urlId = url.parse(req.url).pathname.replace(/^\//, '');
-  var documentId = urlId === null || urlId === '' ? null : ObjectId(urlId);
-
-  let body = [];
-
-  if(method === 'GET') {
-    req.on('data', function(chunk) {
-      body.push(chunk);
-    }).on('end', () => {
-      MongoClient.connect(mongoUrl, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db(mongoBase);
+  MongoClient.connect(mongoUrl, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(mongoBase);
 
       // Creates the minefield instance
       var field = new MineField();
@@ -51,9 +43,8 @@ http.createServer(function (req, res) {
               status.status = 'unmodified';
             }
 
-            var response = JSON.stringify({...status, ...fresh, "data": field});
-            res.write(response);
-            res.end();
+            var response = {...status, ...fresh, "data": field};
+            res.send(response);
           });
       } else {
           // If no id is provided, it saves the minefield instance into the DB
@@ -61,52 +52,47 @@ http.createServer(function (req, res) {
 
           fresh.isFresh = true;
           status.status = 'created';
-          var response = JSON.stringify({...status, ...fresh, "data": field});
-          res.write(response);
-          res.end();
+          var response = {...status, ...fresh, "data": field};
+          res.send(response);
         }
       });
+});
+
+app.post('/:minefieldId?', function(req, res) {
+  let documentId = req.params['minefieldId'] ? ObjectId(req.params['minefieldId']) : null;
+
+  // Removes the _id field from the data JSON to avoid immutable field error update on Mongo (in case that it would be present)
+  delete req.body.data._id;
+
+  MongoClient.connect(mongoUrl, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db(mongoBase);
+    var myquery = {_id: documentId};
+    var newvalues = { $set: req.body.data };
+
+    dbo.collection(mongoCollection).updateOne(myquery, newvalues, function(err, res) {
+      if (err) throw err;
+
+      if(res.modifiedCount) {
+        status.status = 'updated';
+      } else {
+        status.status = 'unmodified';
+      }
     });
-  } else if(method === 'POST') {
-    req.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      // Assigns all received data to variable body as json
-      body = JSON.parse(Buffer.concat(body).toString());
-      
-      // Gets the id to be updated from the body, an removes it from the json
-      delete body.data._id;
-      
-      MongoClient.connect(mongoUrl, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db(mongoBase);
-        var myquery = {_id: documentId};
-        var newvalues = { $set: body.data };
-        
-        dbo.collection(mongoCollection).updateOne(myquery, newvalues, function(err, res) {
-          if (err) throw err;
 
-          if(res.modifiedCount) {
-            status.status = 'updated';
-          } else {
-            status.status = 'unmodified';
-          }
-        });
+    dbo.collection(mongoCollection).findOne({_id: documentId}).then((dbDocument) => {
+      field = dbDocument;
+      fresh.isFresh = false;
+      var response = {...status, ...fresh, "data": field};
+      res.send(response);
+    })
+  });
+});
 
-        dbo.collection(mongoCollection).findOne({_id: documentId}).then((dbDocument) => {
-          field = dbDocument;
-          fresh.isFresh = false;
-          var response = JSON.stringify({...status, ...fresh, "data": field});
-          res.write(response);
-          res.end();
-        })
-      });
-    });    
-  } else {
-    res.write('Method not implemented');
-    res.end();
-  }
-}).listen(8080);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
 
 
 function getNewDocument(db, dbo, field) {
